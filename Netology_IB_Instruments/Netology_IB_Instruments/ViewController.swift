33d2e9f56708c7cad04e7d53bdeb21338b11b03d
Vladlen Morozov <chapouis@yandex.ru>
2021-08-23 21:28:26 +0300

    New project added

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Владлен Морозов on 23.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
