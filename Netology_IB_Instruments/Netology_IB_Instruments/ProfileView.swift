22dca6833c890ebdcc4959944fc9a7bacf1f3cc0
Vladlen Morozov <chapouis@yandex.ru>
2021-08-23 21:37:02 +0300

    Xib with UI elements, ProfileVIew with IBO outlets added

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Владлен Морозов on 23.08.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var avatarView: UIImageView!
+    @IBOutlet weak var nameView: UILabel!
+    @IBOutlet weak var cityView: UILabel!
+    @IBOutlet weak var birthDateView: UILabel!
+    @IBOutlet weak var descriptionView: UITextView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
